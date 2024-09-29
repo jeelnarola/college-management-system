e8e9f4790ee8e52a9f089cc97db0482f35c00c34
jeel narola <narolajeel29@gmail.com>
2024-09-29 09:47:26 +0530

    Course Add Popup Design

--- src/checkLogin.js
+++ src/checkLogin.js
@@ -1,8 +1,9 @@
-import  {Login}  from "./Components/load.js";
+import  {asideHeader, Login}  from "./Components/load.js";
 
 
-
+document.getElementById("AdminHeader").innerHTML=asideHeader()
 const LoginValue = sessionStorage.getItem('Username')
+
 // DONE
 // if (LoginValue == null){
 //     document.getElementById("Login").innerHTML=Login()
